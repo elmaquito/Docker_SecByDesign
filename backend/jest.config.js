86e74e44e2e2fa48f2e0fa81813f33692fef933e
elmaquito <elmaquito97@gmail.com>
2026-04-01 12:20:31 +0200

    ci: update Jest configuration to include coverage settings

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests', '<rootDir>/src'],
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/main.ts',
+    '!src/**/*.test.ts',
+  ],
 };
